a5b89f3262a7dfe798d8baea123521d58141abd0
gnatal95 <guilherme.natal47@gmail.com>
2017-03-30 13:58:12 -0300

    main.py
    main called to start program

---
+++ main.py
@@ -0,0 +1,13 @@
+
+#lets define w->up a->left s->down d ->rigt
+#j-> action move k-> jump move l-> special move
+import Game
+
+def main():
+	width = 1024
+	height = 600
+	game = Game.Game()
+	game.Run()
+
+main()
+
